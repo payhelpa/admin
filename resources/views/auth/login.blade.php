50f851ae2e5bdab6ee9395af48eefca5b500730d
payhelpa <93013713+payhelpa@users.noreply.github.com>
2022-05-16 23:13:20 +0300

    Changed login page title to Sign in

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
-<title>PayHelpa - Login</title>
+<title>PayHelpa - Sign in</title>
 
 <link rel="shortcut icon" type="image/x-icon" href="{{asset('assets/img/favicon.png')}}">
 
